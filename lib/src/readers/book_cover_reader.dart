cf0b480d2161099784c21f08a4bec820f4bf16ad
Rich Dodzi Atitsusi <dodzi@Juni-Rich.local>
2023-05-01 15:05:57 +0000

    Resolves null safe and version issues

--- lib/src/readers/book_cover_reader.dart
+++ lib/src/readers/book_cover_reader.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:collection/collection.dart' show IterableExtension;
 import 'package:image/image.dart' as images;
@@ -38,9 +39,10 @@ class BookCoverReader {
     }
 
     coverImageContentFileRef = bookRef.Content!.Images![coverManifestItem.Href];
-    var coverImageContent =
-        await coverImageContentFileRef!.readContentAsBytes();
-    var retval = images.decodeImage(coverImageContent);
+    var coverImageContent = await coverImageContentFileRef!.readContentAsBytes();
+    var coverImageUint8List = Uint8List.fromList(coverImageContent);
+    var retval = images.decodeImage(coverImageUint8List);
     return retval;
+
   }
 }
